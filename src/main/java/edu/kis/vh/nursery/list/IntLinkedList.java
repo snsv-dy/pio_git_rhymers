5a8a0a192df3783a069ccedf8433d29e546abb53
Jacek Stasiak <snsvgb@wp.pl>
2021-01-19 21:32:06 +0100

    1.5.6 - Dodano dokumentację klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -11,13 +11,21 @@ class Node {
 
 }
 
+/**
+ * Prosta lista z dowiązaniami. Operująca na typach int.
+ */
 public class IntLinkedList {
+
 
 	private Node last;
 	private int i;
 	public static final int IF_EMPTY = -1;
 
 
+	/**
+	 * Dodaje element do listy.
+	 * @param i liczba wstawiona na koniec listy
+	 */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -28,20 +36,36 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Sprawdza czy lista jest pusta.
+	 * @return wartość logiczna testu.
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * Sprawdza czy lista jest pełna.
+	 * @return wartość logiczna testu
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * Pokazuje jaka jest wartość na końcu listy bez usuwania go z listy.
+	 * @return wartość elementu na końcu listy.
+	 */
 	public int top() {
 		if (isEmpty())
 			return IF_EMPTY;
 		return last.value;
 	}
 
+	/**
+	 * Usuwa element z listy i zwraca go.
+	 * @return wartość elementu na końcu listy.
+	 */
 	public int pop() {
 		if (isEmpty())
 			return IF_EMPTY;
@@ -50,18 +74,34 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	/**
+	 * Zwraca ostatni węzeł listy.
+	 * @return Obiekt Node z końca listy;
+	 */
 	public Node getLast() {
 		return last;
 	}
 
+	/**
+	 * Ustawia ostatni element listy;
+	 * @param last element który ma być ostatnim.
+	 */
 	public void setLast(Node last) {
 		this.last = last;
 	}
 
+	/**
+	 * Zwraca i.
+	 * @return i
+	 */
 	public int getI() {
 		return i;
 	}
 
+	/**
+	 * Ustawia i.
+	 * @param i
+	 */
 	public void setI(int i) {
 		this.i = i;
 	}
